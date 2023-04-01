6809a40b61433041f16151cf631f07ca4d331bbc
Sasaki-Motoaki <voyegesansretour1@gmail.com>
2023-04-01 18:29:00 +0900

    git rm

--- my-wave-portal/hardhat.config.ts
+++ my-wave-portal/hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/umK0Q1gUnfHv1vuZh8Cw72h9Ja3gWR8I",
+      accounts: [
+        "e89dbffe03303d25c20760599c9018dab2dddb8acece3f37bda9c3be293978c6",
+      ],
+    },
+  },
 };
 
 export default config;
